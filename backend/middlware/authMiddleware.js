0f151e9bcf113474b6cd63d5810fdda70abc2d6f
Adarsh <adarsh9540984202@gmail.com>
2024-04-17 21:48:47 +0530

    first

---
+++ backend/middlware/authMiddleware.js
@@ -0,0 +1,24 @@
+const { JWT_SECRET } = require("../config");
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
+        return res.status(403).json({});
+    }
+
+    const token = authHeader.split(' ')[1]; // Corrected typo from spilt to split
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.userId = decoded.userId;
+        next();
+    } catch (e) {
+        return res.status(403).json({});
+    }
+};
+
+module.exports = {
+    authMiddleware
+};
